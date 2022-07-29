700d59bbf85b110a2d1f43f3c1bee35b8d36283e
samuel.pereira <jose.pereira@tributech.com.br>
2022-07-29 13:57:16 -0300

    hellow word

---
+++ hellow/hello.go
@@ -0,0 +1,8 @@
+package main //indica que esse é o pacote principal
+
+import "fmt" //importa o pacote de formatação
+
+func main() { //função principal, analogo ao int main() em C
+	fmt.Println("hellow word!") // Println, importado na linha 3
+
+}
